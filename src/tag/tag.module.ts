92a5de21085c57446e43285c37e2e68baac252f7
Luis Miguel Sanchez Velazquez <luis.sanchez@musala.com>
2022-12-15 15:56:42 +0200

    Fix bug on tags controller

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TagController } from './tag.controller';
+import { TagService } from './tag.service';
 
 @Module({
+  providers: [TagService],
   controllers: [TagController],
 })
 export class TagModule {}
